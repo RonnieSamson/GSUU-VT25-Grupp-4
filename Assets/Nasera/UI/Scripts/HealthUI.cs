fb7920276ce75dbb550f6c6e74ca02572c29e99c
NaseraBoujrad <boujradnasera@gmail.com>
2025-05-26 23:37:24 +0200

    Lagt till färdig HealthPowerup med blink, max-liv kontroll och visuell feedback
    Ny prefab med hjärta som snurrar.
    Blinkar när spelaren har mindre än max HP.
    kan plockas upp om spelaren har mindre  än 3 liv.

---
+++ Assets/Nasera/UI/Scripts/HealthUI.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class HealthUI : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public TextMeshProUGUI healthText;
+
+    void Update()
+    {
+        if (playerHealth != null && healthText != null)
+        {
+            healthText.text = string.Join(" ", new string('❤', (int)playerHealth.currentHealth).ToCharArray());
+
+        }
+    }
+
+}
