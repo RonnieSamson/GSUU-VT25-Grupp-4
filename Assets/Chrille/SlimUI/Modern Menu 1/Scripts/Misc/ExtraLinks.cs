58c7d090e7403af9a8381f15bca77240399c99cb
Christian Vittaniemi <crv87@outlook.com>
2025-05-28 09:02:37 +0200

    Added "dock thing" scene to mainManu

--- Assets/Chrille/SlimUI/Modern Menu 1/Scripts/Misc/ExtraLinks.cs
+++ Assets/Chrille/SlimUI/Modern Menu 1/Scripts/Misc/ExtraLinks.cs
@@ -2,15 +2,20 @@
 using UnityEngine.SceneManagement;
 
 namespace SlimUI.ModernMenu{
-    
-    
+
+
     public class ExtraLinks : MonoBehaviour
     {
 
-        public string[] scenes = {"Chrille"};
+        public string[] scenes = { "Chrille", "Dock Thing" };
         public void OpenChrille()
         {
             SceneManager.LoadScene(scenes[0]);
+        }
+        
+        public void OpenDockThing()
+        {
+            SceneManager.LoadScene(scenes[1]);
         }
 
     }
